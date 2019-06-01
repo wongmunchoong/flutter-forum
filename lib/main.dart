d92c51e9195ea477783b50227fd1d0019c971781
Alexander Wong Mun Choong <wongmunchoong@gmail.com>
2019-06-01 16:04:30 +0800

    text complete

--- lib/main.dart
+++ lib/main.dart
@@ -48,21 +48,56 @@ class CardList extends StatelessWidget {
       padding: const EdgeInsets.all(16.0),
       physics: PageScrollPhysics(),
       children: <Widget>[
-        StoryCard(),
-        StoryCard(),
-        StoryCard(),
+        StoryCard(
+          image: NetworkImage("https://preview.redd.it/hmaajff9vl131.jpg?width=640&crop=smart&auto=webp&s=e3d2173d06b020b0191fa8583ab4d465ba6c9e20"),
+          profileImage: NetworkImage("https://styles.redditmedia.com/t5_mg9yg/styles/profileIcon_okt08xsvkuy21.jpg?width=256&height=256&crop=256:256,smart&s=7115f8489eefdb8a9ae20cf7006b0e7f04f3b409"),
+          name: "u/SanWhich",
+          title: "That'll show her",
+          category: "Article"),
+        StoryCard(
+          image: NetworkImage("https://i.redd.it/jn8k6jzw4j131.jpg"),
+          profileImage: NetworkImage("https://styles.redditmedia.com/t5_ov2ac/styles/profileIcon_e2hfy5r2eex21.jpg?width=256&height=256&crop=256:256,smart&s=3f35d2f8c4c9da27cd3364bae175852625a95f77"),
+          name: "u/YaSiN0704",
+          title: "shit’s gonna be different now",
+          category: "Article",),
+        StoryCard(
+          image: NetworkImage("https://preview.redd.it/ps64ci8cmi131.jpg?width=640&crop=smart&auto=webp&s=cec635d34efcf6ba8e1f87a18f979ce524e1ca47"),
+          profileImage: NetworkImage("background-image:url(https://www.redditstatic.com/avatars/avatar_default_19_FFB000.png)"),
+          name: "u/fibonacci666",
+          title: "wokeee",
+          category: "Article",),
+        StoryCard(
+          image: NetworkImage("https://i.redd.it/ahzek8xm6l131.jpg"),
+          profileImage: NetworkImage("https://www.redditstatic.com/avatars/avatar_default_11_A5A4A4.png"),
+          name: "u/Been-Thr-Done-Tht",
+          title: "Oh Yeah ..😁",
+          category: "Article",),
       ],
     ));
   }
 }
 
 class StoryCard extends StatelessWidget {
+    final String name;
+  final String title;
+  final String category;
+  final NetworkImage image;
+  final NetworkImage profileImage;
+
+  StoryCard({
+    @required this.name,
+    @required this.title,
+    @required this.category,
+    @required this.image,
+    @required this.profileImage,
+  });
+
   @override
   Widget build(BuildContext context) {
     return Container(
         margin: const EdgeInsets.fromLTRB(0.0, 8.0, 0.0, 8.0),
         height: 500.0,
-        width: 350.0,
+        width: MediaQuery.of(context).size.width * 1,
         decoration: BoxDecoration(
           color: Colors.white,
           borderRadius: BorderRadius.circular(8.0),
@@ -78,27 +113,34 @@ class StoryCard extends StatelessWidget {
           ],
         ),
         child: DeveloperCard(
-          image: AssetImage("assets/Background.png"),
-          name: "asd",
+          image: this.image,
+          profileImage: this.profileImage,
+          name: this.name,
+          title:this.title,
+          category: this.category,
         ));
   }
 }
 
 class DeveloperCard extends StatelessWidget {
   final String name;
-  final AssetImage image;
+  final String title;
+  final String category;
+  final NetworkImage image;
+  final NetworkImage profileImage;
 
   DeveloperCard({
     @required this.name,
+    @required this.title,
+    @required this.category,
     @required this.image,
+    @required this.profileImage,
   });
 
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(
       builder: (context, constraints) {
-        final diameter = constraints.minWidth - 84;
-
         return Stack(
           children: <Widget>[
             new Positioned(
@@ -110,11 +152,10 @@ class DeveloperCard extends StatelessWidget {
                   height: 240.0,
                   decoration: new BoxDecoration(
                       borderRadius: BorderRadius.only(
-                        topRight:Radius.circular(8.0),
-                        topLeft: Radius.circular(8.0), 
-                        bottomLeft: Radius.zero, 
-                        bottomRight: Radius.zero 
-                      ),
+                          topRight: Radius.circular(8.0),
+                          topLeft: Radius.circular(8.0),
+                          bottomLeft: Radius.zero,
+                          bottomRight: Radius.zero),
                       image: new DecorationImage(
                           fit: BoxFit.cover, image: this.image)),
                 )),
@@ -126,13 +167,38 @@ class DeveloperCard extends StatelessWidget {
                   height: 64.0,
                   decoration: new BoxDecoration(
                       border: Border.all(
-                        width: 4,
-                        color: Color.fromARGB(255, 255, 255, 255)
-                      ),
+                          width: 4, color: Color.fromARGB(255, 255, 255, 255)),
                       shape: BoxShape.circle,
                       image: new DecorationImage(
-                          fit: BoxFit.fill, image: this.image))),
-            )
+                          fit: BoxFit.fill, image: this.profileImage))),
+            ),
+            Positioned(
+                left: 0.0,
+                top: 280.0,
+                child: new Container(
+                  alignment: Alignment(0, 0),
+                  width: MediaQuery.of(context).size.width * 0.85,
+                  height: 40.0,
+                  child: new Text(
+                    "${this.name}   ·   5h   ·   ${this.category}",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 14.0),
+                  ),
+                )),
+            Positioned(
+                left: 0.0,
+                top: 350.0,
+                child: new Center(
+child:new Container(
+                  padding: const EdgeInsets.only(left: 10.0, right: 10.0),
+                  width: MediaQuery.of(context).size.width*1,
+                  height: 100.0,
+                  child: new Text(
+                    "${this.title}",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 18.0),
+                  ),
+                )))
           ],
         );
       },
